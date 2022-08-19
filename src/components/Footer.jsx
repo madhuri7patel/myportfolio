277e09501c4a632ddb666d91ac4bcfa98781e257
madhuri7patel <madhuri7patel@gmail.com>
2022-08-19 11:03:31 +0530

    Myportfolio

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <hr></hr>
+      <p className="footer-text">
+        Made by Madhuri Patel<br></br>
+        <br></br>
+        <small>All rights reserved © 2022 Madhuri Patel</small>
+        <br></br>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Home.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { AttentionSeeker } from "react-awesome-reveal";
+// import helloLight from '../media/SushantPortfolio.png';
+// import helloDark from '../media/hello-dark.png';
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/esm/Row";
+import Container from "react-bootstrap/esm/Container";
+
+const Home = ({ darkMode }) => {
+  return (
+    <div>
+      <AttentionSeeker shakeX>
+        <Row>
+          <Container align="center">
+            <Col lg={12} xs={12}>
+              <br />
+
+              <div className="img">
+                <img
+                  src="https://user-images.githubusercontent.com/75351991/184895278-fde86738-e07a-4335-8b83-7a4878664056.png"
+                  alt=""
+                />
+              </div>
+            </Col>
+          </Container>
+        </Row>
+        <Row>
+          <Col lg={12} xs={12}>
+            <h1 className="display-3 hello" align="center">
+              <code> I'm Madhuri Patel</code>
+            </h1>
+            <h2 className="lead" align="center">
+              I
+              <span
+                class="txt-rotate"
+                data-period="500"
+                data-rotate='[ " am a Full-Stack Web Developer"]'
+              ></span>
+            </h2>
+          </Col>
+        </Row>
+      </AttentionSeeker>
+      <br></br>
+      <hr></hr>
+    </div>
+  );
+};
+
+export default Home;
